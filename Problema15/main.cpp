d191b81f2d67adac0a99aff700760d615c66fa25
LuisSepulveda022 <luis.sepulveda1@udea.edu.co>
2018-03-22 20:39:35 -0500

    Problema15.2

--- Problema15/main.cpp
+++ Problema15/main.cpp
@@ -2,23 +2,26 @@
 
 using namespace std;
 
-int* interseccion( int * , int *);  //Funcion que halla la interseccion de dos rectangulos(los valores de cada rectangulo deben ser positivos)
+void interseccion( int * , int * , int *);  //Funcion que halla la interseccion de dos rectangulos(los valores de cada rectangulo deben ser positivos)
+
 
 int main()
 {
-    int a[] ={0,0,8,4},b[]={5,2,6,7}; /*Los primeros 2 datos corresponden a las coordenadas de la esquina superior izquierda del rectángulo (x,y)
-                                        Los siguientes 2 datos representan el ancho(A) y la altura(H) del rectángulo.*/
-    //int interse[4];
-    int *inter;
-    inter = interseccion(a,b); //utilizamos la funcion para hallar la interseccion respectiva
+    int rectangulo1[] ={0,0,8,4},rectangulo2[]={5,2,6,7}; /*Los primeros 2 datos corresponden a las coordenadas de la esquina superior izquierda del rectángulo (x,y)
+                                                            Los siguientes 2 datos representan el ancho(A) y la altura(H) del rectángulo.*/
+
+    int *rectangulo3 = new int[4]; //en rectangulo3 se guardan los valores de la interseccion de los rectangulos dados
+
+     interseccion(rectangulo1,rectangulo2,rectangulo3); //utilizamos la funcion para hallar la interseccion respectiva
 
     //mostramos los valores hallados en la funcion
-    cout << inter[0] << " " << inter[1] << " " << inter[2] << " " << inter[3] << endl;
+    cout <<"La interseccion del rectangulo1={"<<rectangulo1[0]<<","<<rectangulo1[1]<<","<<rectangulo1[2]<<","<<rectangulo1[3]<<"} y el rectangulo2={"<<rectangulo2[0]<<","<<rectangulo2[1]<<","<<rectangulo2[2]<<","<<rectangulo2[3]<<"}\n";
+    cout <<"Es={"<<rectangulo3[0]<<","<<rectangulo3[1]<<","<<rectangulo3[2]<<","<<rectangulo3[3]<<"}\n";
     return 0;
 }
 
-int * interseccion(int *recta1 , int *recta2){
-    int *recta3 = new int[4];                   //Creamos el arreglo que vamos ha devolver con los datos
+void interseccion(int *recta1 , int *recta2, int *recta3){
+    //int *recta3 = new int[4];                   //Creamos el arreglo que vamos ha devolver con los datos
     int x1,y1,A1,H1,x2,y2,A2,H2;    //Definimos las variables de cada aspecto del rectangulo,sus coordenadas(x,y),ancho(A) y altura(H)
 
     //le damos los valores ha cada variable segun la posicion en el arreglo ingresado
@@ -45,7 +48,7 @@ int * interseccion(int *recta1 , int *recta2){
         if (recta3[0] < (x1+A1))
             recta3[2] = (x1+A1)-recta3[0];
     }
-    //cout << x3 << endl;
+
     if(y1 > y2){
         if(y1 < (H2+y2))
             recta3[1] = y1;
@@ -58,7 +61,4 @@ int * interseccion(int *recta1 , int *recta2){
             recta3[3] = (y1+H1)-recta3[1];
     }
 
-    //cout << interse[0] << " " << interse[1] << " " << interse[2] << " " << interse[3] << endl;
-
-    return recta3;
 }
