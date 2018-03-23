6478b4ab031430b2c3343d29a08fe3685c474f1b
LuisSepulveda022 <luis.sepulveda1@udea.edu.co>
2018-03-22 20:14:21 -0500

    Problema15.1

--- Problema15/main.cpp
+++ Problema15/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int* interseccion( int * , int *);  //Funcion que halla la interseccion de dos rectangulos(los valores de cada rectangulo deben ser positivos)
+
 int main()
 {
     int a[] ={0,0,8,4},b[]={5,2,6,7}; /*Los primeros 2 datos corresponden a las coordenadas de la esquina superior izquierda del rectángulo (x,y)
@@ -18,7 +19,7 @@ int main()
 
 int * interseccion(int *recta1 , int *recta2){
     int *recta3 = new int[4];                   //Creamos el arreglo que vamos ha devolver con los datos
-    int x1,y1,A1,H1,x2,y2,A2,H2,x3,y3,A3,H3;    //Definimos las variables de cada aspecto del rectangulo,sus coordenadas(x,y),ancho(A) y altura(H)
+    int x1,y1,A1,H1,x2,y2,A2,H2;    //Definimos las variables de cada aspecto del rectangulo,sus coordenadas(x,y),ancho(A) y altura(H)
 
     //le damos los valores ha cada variable segun la posicion en el arreglo ingresado
     //Rectangulo1
@@ -34,33 +35,29 @@ int * interseccion(int *recta1 , int *recta2){
 
     if(x1 > x2){
         if(x1 < (A2+x2))
-            x3 = x1;
-        if(x3 < (x2+A2))
-             A3 = (x2+A2)-x3;
+            recta3[0] = x1;
+        if(recta3[0] < (x2+A2))
+             recta3[2] = (x2+A2)-recta3[0];
 
     }else if(x2 > x1){
         if(x2 < (A1+x1))
-            x3=x2;
-        if (x3 < (x1+A1))
-            A3 = (x1+A1)-x3;
+            recta3[0]=x2;
+        if (recta3[0] < (x1+A1))
+            recta3[2] = (x1+A1)-recta3[0];
     }
     //cout << x3 << endl;
     if(y1 > y2){
         if(y1 < (H2+y2))
-            y3 = y1;
-        if(y3 < (y2+H2))
-                H3 = (y2+H2)-y3;
+            recta3[1] = y1;
+        if(recta3[1] < (y2+H2))
+                recta3[3] = (y2+H2)-recta3[1];
     }else if(y2 > y1){
         if(y2 < (H1+y1))
-            y3=y2;
-        if (y3 < (y1+H1))
-            H3 = (y1+H1)-y3;
+            recta3[1]=y2;
+        if (recta3[1] < (y1+H1))
+            recta3[3] = (y1+H1)-recta3[1];
     }
 
-    recta3[0]=x3;  //le damos los valores hallados al arreglo que vamos ha devolver
-    recta3[1]=y3;
-    recta3[2]=A3;
-    recta3[3]=H3;
     //cout << interse[0] << " " << interse[1] << " " << interse[2] << " " << interse[3] << endl;
 
     return recta3;
